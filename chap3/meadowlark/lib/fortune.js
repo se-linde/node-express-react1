20d625d711f549ef0f3af6f5785267e1e29f6adb
Dwayne Linde <lindesara@gmail.com>
2017-08-06 21:36:44 -0700

    final chap 3.
    .

--- chap3/meadowlark/lib/fortune.js
+++ chap3/meadowlark/lib/fortune.js
@@ -1,12 +1,9 @@
 var fortuneCookies = [
-    "AAAAAAAAAAAAA",
-    "BBBBBBBBBBBBB",
-    "CCCCCCCCCCCCC",
-    "DDDDDDDDDDDDD",
-    "EEEEEEEEEEEEE",
-    "FFFFFFFFFFFFF",
-    "GGGGGGGGGGGGG",
-    "HHHHHHHHHHHHH"
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
 ]; 
 
 exports.getFortune = function() {
